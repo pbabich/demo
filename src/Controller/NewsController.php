43bcc03be9212dd2d50e3cfe4bb358f2d9b53ea6
Pavel Babich <pavel.babich@platform.io>
2020-09-17 18:14:50 +0300

    add paginator

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -6,16 +6,38 @@ use App\Entity\News;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class NewsController extends AbstractController
 {
     /**
      * @Route("/news", name="news")
      */
-    public function index()
+    public function index(Request $request, PaginatorInterface $paginator)
     {
+        // Retrieve the entity manager of Doctrine
+        $em = $this->getDoctrine()->getManager();
+
+        // Get some repository of data, in our case we have an News entity
+        $newsRepository = $em->getRepository(News::class);
+
+        // Find all the data on the News table, filter your query as you need
+        $allNewsQuery = $newsRepository->createQueryBuilder('n')->getQuery();
+
+        // Paginate the results of the query
+        $news = $paginator->paginate(
+        // Doctrine Query, not results
+            $allNewsQuery,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            2
+        );
+
+        // Render the twig view
         return $this->render('news/index.html.twig', [
-            'controller_name' => 'NewsController',
+            'news' => $news
         ]);
     }
 
